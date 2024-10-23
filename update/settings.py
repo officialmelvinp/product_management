0fd052fe5618e649dd3d8ba1ab3efa33d1f9ec3d
officialmelvinp <elmelvin@2002@gmail.com>
2024-10-23 03:41:21 +0100

    updated settings for static files

--- update/settings.py
+++ update/settings.py
@@ -84,7 +84,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
